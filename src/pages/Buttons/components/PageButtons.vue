3ee21e32eff8181b0ba972877945b92001e4a69b
lifiij <migoviprog@gmail.com>
2023-09-23 18:49:15 +0400

    added buttons

--- src/pages/Buttons/components/PageButtons.vue
+++ src/pages/Buttons/components/PageButtons.vue
@@ -1,7 +1,65 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ButtonDefault from '@/ui/buttons/ButtonDefault.vue'
+
+const eventForButton = () => {
+  alert('You clicked danger button')
+}
+</script>
 
 <template>
   <h1 class="heading-1">Buttons</h1>
+  <h2 class="heading-2">Disabled</h2>
+  <div class="line">
+    <ButtonDefault label="Primary" color="primary" disabled />
+    <ButtonDefault label="Secondary" color="second" disabled />
+    <ButtonDefault label="Success" color="success" disabled />
+    <ButtonDefault label="Info" color="info" disabled />
+    <ButtonDefault label="Warning" color="warning" disabled />
+    <ButtonDefault label="Danger" color="danger" disabled />
+  </div>
+  <h2 class="heading-2">Colors</h2>
+  <div class="line">
+    <ButtonDefault label="Primary" color="primary" />
+    <ButtonDefault label="Secondary" color="second" />
+    <ButtonDefault label="Success" color="success" />
+    <ButtonDefault label="Info" color="info" />
+    <ButtonDefault label="Warning" color="warning" />
+    <ButtonDefault label="Danger" color="danger" />
+  </div>
+  <h2 class="heading-2">Rounded</h2>
+  <div class="line">
+    <ButtonDefault label="Primary" color="primary" rounded />
+    <ButtonDefault label="Secondary" color="second" rounded />
+    <ButtonDefault label="Success" color="success" rounded />
+    <ButtonDefault label="Info" color="info" rounded />
+    <ButtonDefault label="Warning" color="warning" rounded />
+    <ButtonDefault label="Danger" color="danger" rounded />
+  </div>
+  <h2 class="heading-2">Outlined</h2>
+  <div class="line">
+    <ButtonDefault label="Primary" color="primary" outlined />
+    <ButtonDefault label="Secondary" color="second" outlined />
+    <ButtonDefault label="Success" color="success" outlined />
+    <ButtonDefault label="Info" color="info" outlined />
+    <ButtonDefault label="Warning" color="warning" outlined />
+    <ButtonDefault label="Danger" color="danger" outlined />
+  </div>
+  <h2 class="heading-2">Icon</h2>
+  <div class="line">
+    <ButtonDefault color="primary" icon="heart" outlined />
+    <ButtonDefault color="second" icon="hand" />
+    <ButtonDefault color="success" icon="address-book" outlined />
+    <ButtonDefault color="info" icon="credit-card" />
+    <ButtonDefault color="warning" icon="lemon" outlined />
+    <ButtonDefault color="danger" icon="hourglass" />
+  </div>
+  <h2 class="heading-2">Size</h2>
+  <div class="line">
+    <ButtonDefault label="Normal" color="primary" />
+    <ButtonDefault label="Large" color="primary" size="large" />
+  </div>
+  <h2 class="heading-2">Event</h2>
+  <div class="line">
+    <ButtonDefault label="@click" color="danger" @click="eventForButton" />
+  </div>
 </template>
-
-<style scoped></style>
---
+++ src/ui/buttons/ButtonDefault.vue
@@ -0,0 +1,138 @@
+<script setup lang="ts">
+defineProps({
+  label: {
+    type: String,
+    default: 'Button'
+  },
+  color: {
+    type: String,
+    default: 'primary'
+  },
+  disabled: {
+    type: Boolean,
+    required: false
+  },
+  rounded: {
+    type: Boolean,
+    required: false
+  },
+  outlined: {
+    type: Boolean,
+    required: false
+  },
+  icon: {
+    type: String,
+    required: false
+  },
+  size: {
+    type: String,
+    default: 'normal'
+  }
+})
+
+const emit = defineEmits(['click'])
+const clickOnButton = () => {
+  emit('click')
+}
+</script>
+
+<template>
+  <button
+    :class="[
+      'btn',
+      `btn_${color}`,
+      { btn_rounded: rounded },
+      { btn_outlined: outlined },
+      { btn_icon: icon },
+      { btn_large: size === 'large' }
+    ]"
+    :disabled="disabled"
+    @click="clickOnButton"
+  >
+    <span v-if="icon">
+      <font-awesome-icon :icon="`fa-regular fa-${icon}`" />
+    </span>
+    <span v-else>{{ label }}</span>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.btn {
+  margin-right: 10px;
+  margin-bottom: 10px;
+  padding: 0 20px;
+  height: 40px;
+  color: #fff;
+  border-radius: 7px;
+  border: none;
+  cursor: pointer;
+  font-size: 15px;
+  transition: 0.2s;
+  &_primary {
+    background: $color-primary;
+    border: 1px solid $color-primary;
+    &:enabled:hover {
+      background: $color-primary-hover;
+    }
+  }
+  &_second {
+    background: $color-second;
+    border: 1px solid $color-second;
+    &:enabled:hover {
+      background: $color-second-hover;
+    }
+  }
+  &_success {
+    background: $color-success;
+    border: 1px solid $color-success;
+    &:enabled:hover {
+      background: $color-success-hover;
+    }
+  }
+  &_info {
+    background: $color-info;
+    border: 1px solid $color-info;
+    &:enabled:hover {
+      background: $color-info-hover;
+    }
+  }
+  &_warning {
+    background: $color-warning;
+    border: 1px solid $color-warning;
+    &:enabled:hover {
+      background: $color-warning-hover;
+    }
+  }
+  &_danger {
+    background: $color-danger;
+    border: 1px solid $color-danger;
+    &:enabled:hover {
+      background: $color-danger-hover;
+    }
+  }
+  &:disabled {
+    opacity: 0.6;
+    cursor: default;
+  }
+  &_rounded {
+    border-radius: 15px;
+  }
+  &_outlined {
+    background: transparent;
+    color: #000;
+    &:hover {
+      color: #fff;
+    }
+  }
+  &_icon {
+    padding: 0;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
+  &_large {
+    height: 48px;
+    padding: 0 30px;
+  }
+}
+</style>
